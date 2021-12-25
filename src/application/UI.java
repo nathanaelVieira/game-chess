1984d5c231e6e5b17631ce0ef87218097fd342de
nathanael <vieira_nathanael@hotmail.com>
2021-12-25 18:12:49 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -19,7 +19,7 @@ public class UI {
 		if (piece == null ) {
 			System.out.print("•"); // alt + 7 = •
 		} else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
 		System.out.print("  ");
 	}
