82ba51a8024e8686ce6a841ee2e1697665490ce6
nathanael <vieira_nathanael@hotmail.com>
2021-12-25 00:17:21 -0300

    Created: application/UI src/chess - Chess layer and print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,16 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-			Board d = new Board(8, 8);
+
+	ChessMatch chessMatch = new ChessMatch();
+	
+	UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
