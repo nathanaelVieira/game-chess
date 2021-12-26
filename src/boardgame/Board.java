26f7cc44e6fcc2e9f0975b7adf02fc15aab362a5
nathanael <vieira_nathanael@hotmail.com>
2021-12-25 22:27:29 -0300

    Board exception and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if ( rows < 1 || columns < 1) {
+			throw new BoardException("Error crating board: there must be at least one row and one column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -15,28 +18,50 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece(int row, int column) {
+		if ( !positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	
+	/** @RetornaPosicao: a posição de uma peça **/
 	public Piece piece(Position position) {
+		if ( !positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece( Piece piece, Position position) {
+		if(thereIsPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	/** @Auxiliar: Vericando posição existente **/
+	private boolean positionExists( int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists( Position position) {
+		/** @Aux **/
+		return positionExists( position.getRow(), position.getColumn());
+	}
+	
+	/** @HáPeca: verifica se há peças no local **/
+	public boolean thereIsPiece( Position position) {
+		if ( !positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		return piece(position) != null;
+	}
+	
+	
 }
---
+++ src/chess/pieces/Horse.java
@@ -0,0 +1,13 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Horse extends ChessPiece {
+
+	public Horse(Board board, Color color) {
+		super(board, color);
+	}
+
+}
